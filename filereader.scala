75c8fc4499a82cb5bfe0ece75f11f852a1f8a4a5
TejasreePai <100678360+TejasreePai@users.noreply.github.com>
2022-07-28 16:23:18 +0530

    Add files via upload

--- filereader.scala
+++ filereader.scala
@@ -1,8 +1,9 @@
-package sparkcodes.practice
+package reader
+import appConstants.Constants
 import java.io.FileNotFoundException
+import java.io.IOException
 import org.apache.spark.sql.{DataFrame, SparkSession}
 object filereader {
-
   def main(args: Array[String]): Unit = {
     val sparkSession: SparkSession = SparkSession.getActiveSession.getOrElse(
       SparkSession.builder
@@ -11,19 +12,23 @@ object filereader {
         .enableHiveSupport()
         .getOrCreate()
     )
-    try{
-    def Reader(format: String, path: String): DataFrame = {
-      var fileDF: DataFrame = sparkSession.read.option("header", "true").format(format).load(path)
-      fileDF;
-    }
-    //testing
+    try {
+      def Reader(format: String, path: String): DataFrame = {
+        var fileDF: DataFrame = sparkSession.read.option("header", "true").format(format).load(path)
+        fileDF
+      }
       // code to access the function should be put here
-      val file: DataFrame = Reader("csv", "data/item/item.csv")
-      file.show()
+      val clickstreamDF: DataFrame = Reader("csv",Constants.CLICKSTREAM_PATH)
+      val itemDF: DataFrame = Reader("csv",Constants.ITEM_DATA_PATH)
+      itemDF.show()
+
     }
     catch {
       case ex: FileNotFoundException => {
         println("File not found Exception")
+      }
+      case ex: IOException => {
+        println("Input / Output Exception")
       }
     }
   }
