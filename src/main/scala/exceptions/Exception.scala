49cda284c276c62bead901ed2b2d1c734173b4d6
Khushboo100 <khushboos1920@gmail.com>
2022-08-01 12:29:13 +0530

    updated exceptions

--- src/main/scala/exceptions/Exception.scala
+++ src/main/scala/exceptions/Exception.scala
@@ -1,5 +1,19 @@
 package exceptions
 
+import org.apache.spark.sql.DataFrame
+
 object Exception {
 
+  def validation(df:DataFrame): Boolean ={
+    val cols=df.columns.toSeq
+    cols.map(colName=>
+      {
+        if(colName.charAt(0)=='_' && colName.charAt(1)=='c' && colName.charAt(2).isDigit){
+          throw new Exception("Null Column Found")
+          null
+        }
+      }
+    )
+    true
+  }
 }
