a2f42b54cb304b41f9876743e30b2db461ab7171
TheOblivisci <69050822+TheOblivisci@users.noreply.github.com>
2022-08-02 02:20:21 +0530

    Update null handling in FileCleanser.scala

--- src/main/scala/cleanser/FileCleanser.scala
+++ src/main/scala/cleanser/FileCleanser.scala
@@ -8,9 +8,34 @@ import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions.{coalesce, col, current_timestamp, desc, exp, lower, row_number, to_timestamp}
 import org.apache.spark.sql.types.BooleanType
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 import scala.collection.mutable.ListBuffer
 
 object FileCleanser {
+  
+  /*********************REMOVING NULLS FROM THE DATASET****************************/
+  //1.removing rows when primary key is null
+  def removeRows(df: DataFrame, primaryColumns:Seq[String]): DataFrame = {
+    val rowEliminatedDf = df.na.drop("any",primaryColumns)
+    rowEliminatedDf
+  }
+
+  //2.filling null values
+  def fillValues(df:DataFrame, primaryColumns:Seq[String], booleanColumns:Seq[String], timestampColumns:Seq[String]):DataFrame = {
+    //filling false
+    val booleanFilledDf:DataFrame = df.na.fill("FALSE":String,booleanColumns)
+
+    //filling current timestamp
+    val currentTime = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm").format(LocalDateTime.now)
+    val timestampFilledDf:DataFrame = booleanFilledDf.na.fill(currentTime:String,timestampColumns)
+
+    //filling unknown
+    val remainingColumns = (df.columns.toSet).diff(((primaryColumns.toSet).union(booleanColumns.toSet)).union(timestampColumns.toSet)).toSeq
+    val unknownFilledDf:DataFrame = timestampFilledDf.na.fill("unknown":String,remainingColumns)
+    unknownFilledDf
+  }
 
   /**************MODIFYING COLUMN DATA TYPES*********************/
   //converts string to timestamp format
@@ -46,63 +71,7 @@ object FileCleanser {
 
   }
 
-  /*********************REMOVING NULLS FROM THE DATASET****************************/
-  //removing null rows when columns matches not null keys
-  def nullRemoval(inputDF: DataFrame, notNullKeys:Seq[String]): DataFrame = {
-    notNullKeys.toList.foreach{ (element: String) => checkExceptions(inputDF, element) }
-    val outputDF = inputDF.na.drop("any",notNullKeys)
-    outputDF
-  }
 
-  //
-  def isBooleanPresent(inputDF:DataFrame):List[String]={
-    if(inputDF.count() == 0){
-      throw DataframeIsEmptyException("The dataframe is empty")
-    }
-    var list = new ListBuffer[String]()
-    inputDF.schema.fields.foreach(f=>if(f.dataType.equals(BooleanType)) {
-      list=list:+f.name}
-    )
-    val finals=list.toList
-    finals
-  }
 
-  //
-  def handleTimeStamp(inputDF:DataFrame): DataFrame ={
-    if(inputDF.count() == 0){
-      throw DataframeIsEmptyException("The dataframe is empty")
-    }
-    val newDf1 = inputDF.withColumn(ApplicationConstants.TIME_STAMP_COL, coalesce(col(ApplicationConstants.TIME_STAMP_COL), current_timestamp()))
-    val outputDF = newDf1.withColumn(ApplicationConstants.TIME_STAMP_COL, to_timestamp(col(ApplicationConstants.TIME_STAMP_COL),ApplicationConstants.INPUT_TIME_STAMP_FORMAT))
-    outputDF
-  }
-
-  //
-  def handleBoolean(inputDF:DataFrame, list:List[String]): DataFrame={
-    list.toList.foreach{ (element: String) => checkExceptions(inputDF, element) }
-    fillBoolean(inputDF,list.isEmpty,list)
-  }
-
-  //
-  def fillBoolean(inputDF:DataFrame, bool: Boolean, list: List[String]): DataFrame = {
-    list.foreach{ (element: String) => checkExceptions(inputDF, element) }
-    if (bool) {
-      inputDF
-    } else {
-      inputDF.na.fill(value = false, list)
-    }
-  }
-
-  //
-  def fillUnknown(inputDF:DataFrame, notNullKeys:Seq[String]): DataFrame ={
-    notNullKeys.toList.foreach{ (element: String) => checkExceptions(inputDF, element) }
-    val col = inputDF.schema.fieldNames.toSeq
-    val colSet=col.toSet
-    val notNullSet=notNullKeys.toSet
-    val flagColSet=isBooleanPresent(inputDF).toSet
-    val nonPrimaries= colSet.diff(notNullSet).diff(flagColSet).toSeq
-    val filledUnknownDataFrame:DataFrame = inputDF.na.fill("unknown",nonPrimaries)
-    filledUnknownDataFrame
-  }
 
 }
