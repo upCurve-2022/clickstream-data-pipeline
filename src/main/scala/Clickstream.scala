5dc7a7c5a2ee5078b9554191145a742d957508d4
Khushboo100 <77659195+Khushboo100@users.noreply.github.com>
2022-08-07 19:46:30 +0530

    update clickstreamData

--- src/main/scala/Clickstream.scala
+++ src/main/scala/Clickstream.scala
@@ -7,22 +7,6 @@ object Clickstream {
 
   val log:Logger=Logger.getLogger(getClass)
   def main(args: Array[String]) {
-/*
-    val sparkSession: SparkSession = SparkSession.getActiveSession.getOrElse(
-      SparkSession.builder
-        .appName("DataEngineeringUpCurve")
-        .master("local[*]")
-        .enableHiveSupport()
-        .getOrCreate()
-    )
-
-    /*Data Cleansing : Null Removal */
-    val clickStreamDataRead=FileReader.readFile(sparkSession,"csv",constants.ApplicationConstants.ClickStreamPath)
-    val nullRemovedData = cleanser.FileCleanser.nullRemovalClickStreamData(clickStreamDataRead,log)
-
-    val itemDataRead=FileReader.readFile(sparkSession,"csv",constants.ApplicationConstants.ItemDataPath)
-    val nullRemovalFromItem= cleanser.FileCleanser.nullRemovalItemData(itemDataRead,log)
-*/
     try{
       service.DataPipeline.execute()
     }
