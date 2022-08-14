b0cb6e95dff7834bb76f8b3540a0d16fa992cd9d
Khushboo100 <khushboos1920@gmail.com>
2022-08-14 15:13:11 +0530

    Update FileCleanser.scala
    update cleanser

--- src/main/scala/cleanser/FileCleanser.scala
+++ src/main/scala/cleanser/FileCleanser.scala
@@ -1,6 +1,8 @@
 package cleanser
 
 import org.apache.spark.sql._
+import org.apache.spark.sql.functions.{coalesce, col, current_timestamp, lit, map_keys}
+
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -10,13 +12,17 @@ object FileCleanser {
     val rowEliminatedDf = df.na.drop("any",primaryColumns)
     rowEliminatedDf
   }
-  def fillCustomValues(df:DataFrame,cols:Seq[String],fillingValue:String):DataFrame={
-    val df1=df.na.fill(fillingValue,cols)
-    df1
+  //filling custom values
+  def fillValues(df:DataFrame,colNameDefaultValue:Map[String,Any]): DataFrame ={
+    val newDF=df.na.fill(colNameDefaultValue)
+    newDF
   }
+  //filling current time stamp
   def fillCurrentTime(df:DataFrame,cols:Seq[String]): DataFrame={
-    val currentTime = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm").format(LocalDateTime.now)
-    val fillTimeDF=df.na.fill(currentTime,cols);
+    val currentTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now)
+    val fillTimeDF=df.withColumn("event_timestamp",coalesce(col("event_timestamp"),lit(currentTime)))
+//    val currentTime = DateTimeFormatter.ofPattern(constants.ApplicationConstants.INPUT_TIME_STAMP_FORMAT).format(LocalDateTime.now)
+//    val timestampFilledDf:DataFrame = df.na.fill(currentTime:String,cols)
     fillTimeDF
   }
 }
