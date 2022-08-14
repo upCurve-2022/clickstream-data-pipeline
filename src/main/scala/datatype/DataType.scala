88bfffeefdc06b7751bbb41b78ef7542977ccdca
Khushboo100 <khushboos1920@gmail.com>
2022-08-14 15:17:08 +0530

    Update DataType.scala
    change datatype

--- src/main/scala/datatype/DataType.scala
+++ src/main/scala/datatype/DataType.scala
@@ -4,13 +4,15 @@ import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 
 object DataType {
-  def convertStoT(df : DataFrame, col_name: String, format : String) : DataFrame = {
-    val new_df = df.withColumn(col_name, to_timestamp(col(col_name),format))
-    new_df
+
+  def stringToTimestamp(inputDF : DataFrame, colName: String, inputFormat : String) : DataFrame = {
+    val outputDF = inputDF.withColumn(colName, to_timestamp(col(colName),inputFormat))
+    outputDF
+  }
+  def colDatatypeModifier(inputDF : DataFrame, colDatatype : List[(String, String)]) : DataFrame = {
+
+    val outputDF = inputDF.select(colDatatype.map{case(c,t) => inputDF.col(c).cast(t)}:_*)
+    outputDF
   }
 
-  def changeDatatype(df : DataFrame, datatype_seq : Seq[(String,String)]) : DataFrame = {
-    val new_df = df.select(datatype_seq.map{case(c,t) => df.col(c).cast(t)}:_*)
-    new_df
-  }
 }
