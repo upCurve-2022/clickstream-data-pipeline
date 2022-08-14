cd85cad74e366e836b1580265bf9b43f2180b629
Khushboo100 <khushboos1920@gmail.com>
2022-08-14 15:22:38 +0530

    update utils

--- src/main/scala/utils/ExceptionMethod.scala
+++ src/main/scala/utils/ExceptionMethod.scala
@@ -7,6 +7,7 @@ import org.apache.spark.sql.functions._
 object ExceptionMethod {
   def validation(df:DataFrame): Boolean ={
     val cols=df.columns.toSeq
+
     cols.map(colName=>
     {
       if(colName.charAt(0)=='_' && colName.charAt(1)=='c' && colName.charAt(2).isDigit){
