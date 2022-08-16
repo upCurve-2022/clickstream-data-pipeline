03df0af756ffd4852b695e505edd5db494fab3db
Gittika <gittikaagarwal@gmail.com>
2022-08-16 22:51:55 +0530

    gittika commit

--- src/main/scala/service/DataPipeline.scala
+++ src/main/scala/service/DataPipeline.scala
@@ -3,105 +3,107 @@ package service
 import cleanser.FileCleanser._
 import com.typesafe.config.Config
 import constants.ApplicationConstants
-import constants.ApplicationConstants.{CLICK_STREAM_INPUT_PATH, CLICK_STREAM_OUTPUT_PATH, FILE_FORMAT, ITEM_DATA_INPUT_PATH, ITEM_OUTPUT_PATH}
+import constants.ApplicationConstants._
 import org.apache.log4j.Logger
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.SparkSession
 import service.FileReader.fileReader
 import service.FileWriter.writeToOutputPath
-import service.FileWriter.fileWriter
+import transform.JoinDatasets.joinDataFrame
 import utils.ApplicationUtils.{configuration, createSparkSession}
 
 object DataPipeline {
-  implicit val spark:SparkSession = createSparkSession()
+  implicit val spark: SparkSession = createSparkSession()
   val appConf: Config = configuration()
   val log: Logger = Logger.getLogger(getClass)
-  val clickStreamInputPath: String =appConf.getString(CLICK_STREAM_INPUT_PATH)
+  val clickStreamInputPath: String = appConf.getString(CLICK_STREAM_INPUT_PATH)
   val itemDataInputPath: String = appConf.getString(ITEM_DATA_INPUT_PATH)
-  val clickStreamOutputPath: String=appConf.getString(CLICK_STREAM_OUTPUT_PATH)
-  val itemDataOutputPath: String=appConf.getString(ITEM_OUTPUT_PATH)
+  val clickStreamOutputPath: String = appConf.getString(CLICK_STREAM_OUTPUT_PATH)
+  val itemDataOutputPath: String = appConf.getString(ITEM_OUTPUT_PATH)
 
 
   def execute(): Unit = {
-    //reading click stream dataset
 
-    /*****************CLICK STREAM DATASET**********************/
+    /** ***************CLICK STREAM DATASET********************* */
+    //reading click stream dataset
     val clickStreamDF = fileReader(clickStreamInputPath, FILE_FORMAT)
 
-    //Removing rows when primary columns are null
-    val rowEliminatedDF = removeRows(clickStreamDF, ApplicationConstants.CLICK_STREAM_NOT_NULL_KEYS)
-
-    //Replacing null in other rows
-
-    //change the data types
-    val timeStampDataTypeDF=datatype.DataType.stringToTimestamp(clickStreamDF,"event_timestamp",constants.ApplicationConstants.INPUT_TIME_STAMP_FORMAT)
-    val changeDataTypeDF=datatype.DataType.colDatatypeModifier(timeStampDataTypeDF,constants.ApplicationConstants.CLICK_STREAM_DATATYPE)
-
-    //eliminate rows on NOT_NULL_COLUMNS
-    val rowEliminatedClickStreamDF = cleanser.FileCleanser.removeRows(changeDataTypeDF,constants.ApplicationConstants.CLICK_STREAM_NOT_NULL_KEYS)
-    // fill time stamp
-    val timeFilledDF=cleanser.FileCleanser.fillCurrentTime(rowEliminatedClickStreamDF,Seq("event_timestamp"))
-    timeFilledDF.show(5)
-    // fill null values
-    val nullFilledClickSteamDF=cleanser.FileCleanser.fillValues(timeFilledDF,constants.ApplicationConstants.COLUMN_NAME_DEFAULT_VALUE_CLICK_STREAM_MAP)
-
-
-    //converting redirection column into lowercase
-    val modifiedDF = toLowercase(nullFilledClickSteamDF, ApplicationConstants.REDIRECTION_COL)
+    //converting string to timestamp format
+    val convertedDF = stringToTimestamp(clickStreamDF, ApplicationConstants.TIME_STAMP_COL, ApplicationConstants.INPUT_TIME_STAMP_FORMAT)
 
     //modifying the data types of the columns of the click stream dataset
-    val modifiedClickStreamDF = colDatatypeModifier(modifiedDF, ApplicationConstants.CLICK_STREAM_DATATYPE)
+    val modifiedClickStreamDF = colDatatypeModifier(convertedDF, ApplicationConstants.CLICK_STREAM_DATATYPE)
+
+    //Removing rows when primary columns are null
+    val rowEliminatedDF = removeRows(modifiedClickStreamDF, ApplicationConstants.CLICK_STREAM_NOT_NULL_KEYS)
+
+    //Replacing null in other rows
+    val timestampFilledDF = fillCurrentTime(rowEliminatedDF, ApplicationConstants.CLICK_STREAM_TIMESTAMP)
+
+
+    val falseFilledDF = fillCustomValues(timestampFilledDF, clickStreamNullFillValues)
+
+    //converting redirection column into lowercase
+    val modifiedDF = toLowercase(falseFilledDF, ApplicationConstants.REDIRECTION_COL)
+
 
     //remove duplicates from the click stream dataset
-    val clickStreamDFWithoutDuplicates = removeDuplicates(modifiedClickStreamDF, ApplicationConstants.CLICK_STREAM_PRIMARY_KEYS, Some(ApplicationConstants.TIME_STAMP_COL))
+    val clickStreamDFWithoutDuplicates = removeDuplicates(modifiedDF, ApplicationConstants.CLICK_STREAM_PRIMARY_KEYS, Some(ApplicationConstants.TIME_STAMP_COL))
 
     //logging information about click stream dataset
     log.warn("Total items in the click stream dataset " + clickStreamDFWithoutDuplicates.count())
 
     //writing the resultant data to a file
-    writeToOutputPath(clickStreamDFWithoutDuplicates, clickStreamOutputPath, ApplicationConstants.FILE_FORMAT)
+    //    writeToOutputPath(clickStreamDFWithoutDuplicates, clickStreamOutputPath, ApplicationConstants.FILE_FORMAT)
 
     /** **************ITEM DATASET*************** */
     //reading item dataset
     val itemDF = fileReader(itemDataInputPath, ApplicationConstants.FILE_FORMAT)
-    //change the data type
-    val dataTypesDF=datatype.DataType.colDatatypeModifier(itemDF,constants.ApplicationConstants.ITEM_DATATYPE)
+    //handling null values for item dataset
+    val rowEliminatedItemDF = removeRows(itemDF, ApplicationConstants.ITEM_NOT_NULL_KEYS)
 
-    //eliminate rows on NOT_NULL_COLUMNS
-    val rowEliminatedItemDF = cleanser.FileCleanser.removeRows(dataTypesDF,constants.ApplicationConstants.ITEM_NOT_NULL_KEYS)
+    //Replacing null in other rows
 
-    //fill null values
-    val nullFilledItemF=cleanser.FileCleanser.fillValues(rowEliminatedItemDF,constants.ApplicationConstants.COLUMN_NAME_DEFAULT_VALUE_ITEM_DATA_MAP)
+    val unknownFilledItemDF = fillCustomValues(rowEliminatedItemDF, itemDataNullFillValues)
+
+    //modifying the data types of the columns of the item dataset
+    val modifiedItemDF = colDatatypeModifier(unknownFilledItemDF, ApplicationConstants.ITEM_DATATYPE)
 
     //remove duplicates from the item dataset
-    val itemDFWithoutDuplicates = removeDuplicates(nullFilledItemF, ApplicationConstants.ITEM_PRIMARY_KEYS, None)
-
-
-    //join the columns
-    val joinedLeftDataFrame = transform.JoinDatasets.joinDataFrame(nullFilledClickSteamDF,nullFilledItemF,"left")
-    joinedLeftDataFrame.filter("department_name is NULL").show(20)
-
-    //fill null values
-    val joinedTransform=cleanser.FileCleanser.fillValues(joinedLeftDataFrame,constants.ApplicationConstants.COLUMN_NAME_DEFAULT_VALUE_ITEM_DATA_MAP)
-
-    //add a new column event_d
-    //extract date from TimeStamp Column
-    val addDateDF=transform.TransformOperations.addDate(joinedTransform)
-    addDateDF.show(10)
-
-    //add Timestamp when the record is loaded into the table
-    val addTimeStampDF=transform.TransformOperations.addTimeStamp(addDateDF)
-    addTimeStampDF.show(10)
+    val itemDFWithoutDuplicates = removeDuplicates(modifiedItemDF, ApplicationConstants.ITEM_PRIMARY_KEYS)
 
     //logging information about item dataset
     log.warn("Total items in the item dataset " + itemDFWithoutDuplicates.count())
 
+    //  joining two datasets
+    val joinedDataframe = joinDataFrame(clickStreamDFWithoutDuplicates, itemDFWithoutDuplicates, join_key, join_type)
+    joinedDataframe.show(joinedDataframe.count().toInt)
+    joinedDataframe.printSchema()
+    joinedDataframe.show()
+val nullHandledJoinTable=fillCustomValues(joinedDataframe,itemDataNullFillValues)
+    // transform
+    val transformJoinedDF = transform.JoinDatasets.transformDataFrame(nullHandledJoinTable)
+    transformJoinedDF.show()
+
+
+    //testing
+    val df = transformJoinedDF.filter(transformJoinedDF.col("department_name") === "unknown" && transformJoinedDF.col("product_type") === "unknown" && transformJoinedDF.col("vendor_id") === (-1) &&
+      transformJoinedDF.col("item_price") === (-1))
+    transformJoinedDF.printSchema()
     //writing the resultant data of item dataset to a file
     writeToOutputPath(itemDFWithoutDuplicates, itemDataOutputPath, ApplicationConstants.FILE_FORMAT)
 
 
-     //final df to be inserted - write into table
+    //final df to be inserted - write into table
     //demo table
     //fileWriter("table2", itemDFWithoutDuplicates)
   }
+
+
+
+//    timestampFilledDF.show(10)
+
+
+
+
 
 }
