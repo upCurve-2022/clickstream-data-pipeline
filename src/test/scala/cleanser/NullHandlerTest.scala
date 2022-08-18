fc57605fd81b76ab14987bc4e7111868b60db6d2
TheOblivisci <snehasreelata2000@gmail.com>
2022-08-18 16:55:31 +0530

    Create encryptPassword() and decryptPassword()

---
+++ src/test/scala/cleanser/NullHandlerTest.scala
@@ -0,0 +1,5 @@
+package cleanser
+
+class NullHandlerTest {
+
+}
---
+++ src/test/scala/service/FileWriterTest.scala
@@ -0,0 +1,15 @@
+package service
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class FileWriterTest extends AnyFlatSpec{
+
+  "Encrypt Password" should "take the password and encrypted it" in {
+
+  }
+
+  "File Writer" should "take a dataframe and store it as a table in a database " in {
+
+  }
+
+}
