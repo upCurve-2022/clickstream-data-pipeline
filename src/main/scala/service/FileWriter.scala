7d9d321199fa313c312700c5defec25ca4e4bb42
Gittika <gittikaagarwal@gmail.com>
2022-08-24 17:44:59 +0530

    changed

--- src/main/scala/service/FileWriter.scala
+++ src/main/scala/service/FileWriter.scala
@@ -1,20 +1,53 @@
 package service
 
-import exceptions.Exceptions.{DataframeIsEmptyException, FileWriterException}
 import org.apache.spark.sql.DataFrame
+
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+import scala.io.Source
 
 object FileWriter {
 
   //creates a file to store the data of a dataframe in a specified format
-  def writeToOutputPath(inputDF: DataFrame, filePath: String, fileFormat: String): Unit = {
+//  def writeToOutputPath(inputDF: DataFrame, filePath: String, fileFormat: String): Unit = {
+//
+//    if (inputDF.count() == 0) {
+//      throw DataframeIsEmptyException("The dataframe is empty")
+//    }
+//    try {
+//      inputDF.repartition(1).write.format(fileFormat).option("header", "true").mode("overwrite").save(filePath)
+//    } catch {
+//      case e: Exception => throw FileWriterException("Error in writing file to given location")
+//    }
+//  }
 
-    if (inputDF.count() == 0) {
-      throw DataframeIsEmptyException("The dataframe is empty")
-    }
+
+
+  def decryptPassword(encryptedPasswordPath:String):String={
+    //read from file and decrypt password
+    val decryptedPassword = Source.fromFile(encryptedPasswordPath).getLines.mkString
+    val decoded = Base64.getDecoder.decode(decryptedPassword)
+
+    //return the decrypted password as a string
+    val password = new String(decoded, StandardCharsets.UTF_8)
+    password
+  }
+
+  def fileWriter(databaseURL:String,tableName: String, df: DataFrame): Unit = {
+    //change in conf
+
+    val password = decryptPassword(constants.ApplicationConstants.ENCRYPTED_DATABASE_PASSWORD)
     try {
-      inputDF.repartition(1).write.format(fileFormat).option("header", "true").mode("overwrite").save(filePath)
-    } catch {
-      case e: Exception => throw FileWriterException("Error in writing file to given location")
+        df.write.format("jdbc")
+          .option("url", databaseURL)
+          .option("driver", "com.mysql.jdbc.Driver")
+          .option("dbtable", tableName)
+          .option("user", "root")
+          .option("password",password)
+          .mode("overwrite")
+          .save()
+    }catch{
+      case e: Exception => e.printStackTrace()
     }
   }
 }
